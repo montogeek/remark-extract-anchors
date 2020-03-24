0cf8b7497d6b9b9baf74ebb26a47bc2ada338ebe
chenxsan <chenxsan@gmail.com>
2020-03-24 21:09:49 +0800

    Support levels option

--- src/index.js
+++ src/index.js
@@ -13,13 +13,19 @@ module.exports = function extractAnchors(options = {}) {
     throw new Error("Missing or malformed `anchors` in options.");
   }
 
+  if (typeof levels !== "undefined") {
+    if (!Number.isInteger(levels)) {
+      throw new Error("`levels` should only be integer")
+    }
+  }
+
   return function transformer(ast) {
     visit(ast, "heading", visitor);
   };
 
   function visitor(node) {
     if (!node.children.some(child => child.type === "link")) {
-      // TODO: Default header `levels` and check it to filter the `push`ed anchors
+      if (node.depth > levels) return
       options.anchors.push({
         title: node.children.length ? node.children[0].value : "",
         id: node.data.id
